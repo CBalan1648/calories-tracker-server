4c5797ebb03f3cf6f02a1a2d467afc6153975f51
Catalin Balan <catalin.balan@iquestgroup.com>
2019-11-18 17:44:32 +0200

    Add meals schema

---
+++ src/meals/meals.schema.ts
@@ -0,0 +1,8 @@
+import * as mongoose from 'mongoose';
+
+export const MealsSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String },
+    time: { type: Number, required: true },
+    calories: { type: Number, required: true },
+});

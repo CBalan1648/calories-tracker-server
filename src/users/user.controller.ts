f642e65ac377c8cb5985bdc677a43fa64130489e
Catalin Balan <catalin.balan@iquestgroup.com>
2019-11-18 16:52:07 +0200

    Add basic users (register, login, getAll) API endpoint

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,19 +1,26 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { User } from './user.model';
 import { UserService } from './user.service';
+import { UserCredentials } from './userCredentials.model';
 
 @Controller('users')
 export class UserController {
 
     constructor(private readonly userService: UserService) { }
 
+    @Post()
+    async createUser(@Body() user: User) {
+        return this.userService.create(user);
+    }
+
+    @Post('login')
+    async login(@Body() credentials: UserCredentials) {
+        return this.userService.loginUser(credentials);
+    }
+
     @Get()
     async getAllUsers() {
         return this.userService.findAll();
     }
 
-    @Post()
-    async addUser(@Body() user: User) {
-        return this.userService.create(user);
-    }
 }

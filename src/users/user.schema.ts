43591b30c33583dbca7a0fe526b185d36dc206de
Catalin Balan <catalin.balan@iquestgroup.com>
2019-11-20 17:15:49 +0200

    Minor update to user schema

--- src/users/user.schema.ts
+++ src/users/user.schema.ts
@@ -5,7 +5,7 @@ export const UserSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, unique: true, required: true },
-    targetCalories: { type: Number },
+    targetCalories: { type: Number, default: 0 },
     authLevel: { type: String, enum: ['USER', 'USER_MANAGER', 'ADMIN'], default: 'USER' },
     password: { type: String, required: true },
     meals: { type: [MealsSchema], default: [] },

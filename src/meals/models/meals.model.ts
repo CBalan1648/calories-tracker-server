9bfac6aa7093e4248e41f2dd060e1682eac19475
Catalin Balan <catalin.balan@iquestgroup.com>
2019-12-16 14:05:57 +0200

    Minor linting

--- src/meals/models/meals.model.ts
+++ src/meals/models/meals.model.ts
@@ -1,20 +1,24 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-
+import { IsMongoId, IsNotEmpty, IsNumber } from 'class-validator';
 
 // TODO : Add pipe validation;
 export class Meal {
     @ApiPropertyOptional()
+    @IsMongoId()
     _id?: string;
 
     @ApiProperty()
+    @IsNotEmpty()
     title: string;
 
     @ApiPropertyOptional()
     description?: string;
 
     @ApiProperty()
+    @IsNumber()
     time: number;
 
     @ApiProperty()
+    @IsNumber()
     calories: number;
 }

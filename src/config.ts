5bdd012db34201187f3a11c9ae5f9f373992997e
Catalin Balan <cbalan1648@gmail.com>
2020-03-01 20:18:55 +0200

    Test push

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const MONGO_DB_IN_MEMORY_ADDRESS = 'mongodb://localhost';
 export const MINIMUM_PASS_LENGHT = 12;
 
 export const ROOT_ADDRESS = 'api';
- 
+
 // !important : This should be taken from ENV
 
 export const JWT_SECRET = 'HELLO_HELLO_DO_NOT_USE_THIS';

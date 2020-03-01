e5821be659c033fe52bf8dd309c3f7d30ba47353
Catalin Balan <cbalan1648@gmail.com>
2020-03-01 12:43:49 +0200

    Drone integration test - 4 - Add Dockerbuild and publish to private repo

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const SERVER_PORT = 3000;
-export const MONGO_DB_ADDRESS = 'mongodb://localhost:27017';
+export const MONGO_DB_ADDRESS = 'mongodb://mongodb:27017';
 export const MONGO_DB_IN_MEMORY_ADDRESS = 'mongodb://localhost';
 export const MINIMUM_PASS_LENGHT = 12;
 
@@ -10,4 +10,4 @@ export const ROOT_ADDRESS = 'api';
 export const JWT_SECRET = 'HELLO_HELLO_DO_NOT_USE_THIS';
 export const JWT_EXPIRATION_TIME = '24h';
 
-export const CORS_ORIGIN = 'http://localhost:4200';
+export const CORS_ORIGIN = 'http://localhost';

8497a94e6ed4f7e7f6fa2585eafa9f7eb65dd189
Catalin Balan <cbalan1648@gmail.com>
2020-03-11 13:47:02 +0200

    Change CORS origin to HTTPS

--- src/config.ts
+++ src/config.ts
@@ -10,4 +10,4 @@ export const ROOT_ADDRESS = 'api';
 export const JWT_SECRET = 'HELLO_HELLO_DO_NOT_USE_THIS';
 export const JWT_EXPIRATION_TIME = '24h';
 
-export const CORS_ORIGIN = 'http://ctf.azurelynx.com';
+export const CORS_ORIGIN = 'https://ctf.azurelynx.com';
